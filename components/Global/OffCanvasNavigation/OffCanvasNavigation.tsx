13aaf5427974999d4a0d67b6ed7db869ce906fdc
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2022-12-15 16:04:47 -0600

    mobile nav + sub nav styles/logic

--- components/Global/OffCanvasNavigation/OffCanvasNavigation.tsx
+++ components/Global/OffCanvasNavigation/OffCanvasNavigation.tsx
@@ -1,98 +1,163 @@
+import { useState } from 'react'
 import Link from 'next/link'
-import { TbX } from 'react-icons/tb'
+import SocialIcons from '../SocialIcons/SocialIcons'
+import { TbChevronDown, TbX } from 'react-icons/tb'
 
 import styles from './OffCanvasNavigation.module.css'
 
+// TODO: break out into seperate components, needs to be dynamic => map through data and create <NavItem />
 export default function OffCanvasNavigation({
   handleShowMobileNav,
   mobileNavIsActive,
 }) {
+  const [subNavOneIsShown, setSubNavOneIsShown] = useState(false)
+  const [subNavTwoIsShown, setSubNavTwoIsShown] = useState(false)
+  const [subNavThreeIsShown, setSubNavThreeIsShown] = useState(false)
+
+  // Handle hide/show sub navigation
+  const handleShowSubNavOne = () => setSubNavOneIsShown(!subNavOneIsShown)
+  const handleShowSubNavTwo = () => setSubNavTwoIsShown(!subNavTwoIsShown)
+  const handleShowSubNavThree = () => setSubNavThreeIsShown(!subNavThreeIsShown)
+
+  // Canvas container styles
+  const canvasContainerStyles = mobileNavIsActive
+    ? `${styles.offCanvasContainer} ${styles.active}`
+    : styles.offCanvasContainer
+
+  // Chevron styles
+  const subNavOneArrowStyles = subNavOneIsShown
+    ? `${styles.arrow} ${styles.active}`
+    : styles.arrow
+  const subNavTwoArrowStyles = subNavTwoIsShown
+    ? `${styles.arrow} ${styles.active}`
+    : styles.arrow
+  const subNavThreeArrowStyles = subNavThreeIsShown
+    ? `${styles.arrow} ${styles.active}`
+    : styles.arrow
+
+  // Sub navigation styles
+  const subNavOneStyles = subNavOneIsShown
+    ? `${styles.subNav} ${styles.active}`
+    : styles.subNav
+  const subNavTwoStyles = subNavTwoIsShown
+    ? `${styles.subNav} ${styles.active}`
+    : styles.subNav
+  const subNavThreeStyles = subNavThreeIsShown
+    ? `${styles.subNav} ${styles.active}`
+    : styles.subNav
+
   return (
-    <div
-      className={
-        mobileNavIsActive
-          ? `${styles.container} ${styles.active}`
-          : styles.container
-      }
-    >
-      <ul className={styles.mainNav}>
-        <li className={styles.navItem}>
-          <Link href="/diys" onClick={handleShowMobileNav}>
-            DIYs
-          </Link>
-          {/* Sub navigation */}
-          <ul className={styles.subNav}>
-            <li>
-              <Link href="/tags/fabric" onClick={handleShowMobileNav}>
-                Fabric
+    <div className={canvasContainerStyles}>
+      <div className={styles.wrapper}>
+        <ul>
+          <li className={styles.navItem}>
+            <div>
+              <Link href="/diys" onClick={handleShowMobileNav}>
+                DIYs
               </Link>
-            </li>
-            <li>
-              <Link href="/tags/ink" onClick={handleShowMobileNav}>
-                Ink
+              <TbChevronDown
+                className={subNavOneArrowStyles}
+                onClick={handleShowSubNavOne}
+              />
+            </div>
+            {/* Sub navigation */}
+            <ul className={subNavOneStyles}>
+              <li>
+                <Link href="/tags/fabric" onClick={handleShowMobileNav}>
+                  Fabric
+                </Link>
+              </li>
+              <li>
+                <Link href="/tags/ink" onClick={handleShowMobileNav}>
+                  Ink
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li className={styles.navItem}>
+            <div>
+              <Link href="/recipes" onClick={handleShowMobileNav}>
+                Recipes
               </Link>
-            </li>
-          </ul>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/recipes" onClick={handleShowMobileNav}>
-            Recipes
-          </Link>
-          {/* Sub navigation */}
-          <ul className={styles.subNav}>
-            <li>
-              <Link href="/tags/lunch" onClick={handleShowMobileNav}>
-                Lunch
+              <TbChevronDown
+                className={subNavTwoArrowStyles}
+                onClick={handleShowSubNavTwo}
+              />
+            </div>
+            {/* Sub navigation */}
+            <ul className={subNavTwoStyles}>
+              <li>
+                <Link href="/tags/lunch" onClick={handleShowMobileNav}>
+                  Lunch
+                </Link>
+              </li>
+              <li>
+                <Link href="/tags/snacks" onClick={handleShowMobileNav}>
+                  Snacks
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li className={styles.navItem}>
+            <div>
+              <Link href="/bake-off" onClick={handleShowMobileNav}>
+                Bake Off
               </Link>
-            </li>
-            <li>
-              <Link href="/tags/snacks" onClick={handleShowMobileNav}>
-                Snacks
+              <TbChevronDown
+                className={subNavThreeArrowStyles}
+                onClick={handleShowSubNavThree}
+              />
+            </div>
+            {/* Sub navigation */}
+            <ul className={subNavThreeStyles}>
+              <li>
+                <Link
+                  href="/tags/about-my-great-british-baking-project"
+                  onClick={handleShowMobileNav}
+                >
+                  About My Great British Baking Project
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/tags/signature-bakes"
+                  onClick={handleShowMobileNav}
+                >
+                  Signature Bakes
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/tags/technical-bakes"
+                  onClick={handleShowMobileNav}
+                >
+                  Technical Bakes
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/tags/showstopper-bakes"
+                  onClick={handleShowMobileNav}
+                >
+                  Showstopper Bakes
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li className={styles.navItem}>
+            <div>
+              <Link href="/about" onClick={handleShowMobileNav}>
+                About
               </Link>
-            </li>
-          </ul>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/bake-off" onClick={handleShowMobileNav}>
-            Bake Off
-          </Link>
-          {/* Sub navigation */}
-          <ul className={`${styles.subNav} ${styles.right}`}>
-            <li>
-              <Link
-                href="/tags/about-my-great-british-baking-project"
-                onClick={handleShowMobileNav}
-              >
-                About My Great British Baking Project
-              </Link>
-            </li>
-            <li>
-              <Link href="/tags/signature-bakes" onClick={handleShowMobileNav}>
-                Signature Bakes
-              </Link>
-            </li>
-            <li>
-              <Link href="/tags/technical-bakes" onClick={handleShowMobileNav}>
-                Technical Bakes
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/tags/showstopper-bakes"
-                onClick={handleShowMobileNav}
-              >
-                Showstopper Bakes
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/about" onClick={handleShowMobileNav}>
-            About
-          </Link>
-        </li>
-      </ul>
-      <TbX onClick={handleShowMobileNav} />
+            </div>
+          </li>
+        </ul>
+        {/* Close navigation icon */}
+        <TbX className={styles.close} onClick={handleShowMobileNav} />
+      </div>
+      <div className={styles.socialIcons}>
+        <SocialIcons darkMode={true} />
+      </div>
     </div>
   )
 }
