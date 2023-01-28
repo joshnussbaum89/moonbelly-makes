02d91d896c637609b5ac1e069321030476ff9066
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-01-28 15:01:04 -0600

    replace footer logo with Logo component

--- components/Global/Footer/Footer.js
+++ components/Global/Footer/Footer.js
@@ -1,12 +1,9 @@
 // Components
-import Image from 'next/image'
+import Logo from '../Logo/Logo'
 import SocialIcons from '../SocialIcons/SocialIcons'
 
 // Styles
 import styles from './Footer.module.css'
-
-// Images
-import logo from '../../../public/logo.svg'
 
 /**
  * Footer Component
@@ -19,13 +16,7 @@ export default function Footer() {
       {/* TODO */}
       {/* <section className={styles.instagram}></section> */}
       <section className={styles.info}>
-        <Image
-          src={logo}
-          width={770}
-          height={256}
-          alt="Moonbelly Makes site logo"
-          priority
-        />
+        <Logo />
         <SocialIcons darkMode={true} />
         <p>
           created and maintained by moonbelly makes © {new Date().getFullYear()}
