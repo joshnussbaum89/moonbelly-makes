1d6a4aef141796e2b32c4f5804f218c36de03c83
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-03-20 15:17:58 -0500

    fix youtube iframe border, header to h1

--- components/Global/PageTitle/PageTitle.tsx
+++ components/Global/PageTitle/PageTitle.tsx
@@ -5,5 +5,5 @@ import styles from './PageTitle.module.css'
  * Styled page header
  */
 export default function PageTitle({ text }: { text: string }) {
-  return <h2 className={styles.title}>{text}</h2>
+  return <h1 className={styles.title}>{text}</h1>
 }
