11c0b6c241b06dd28248a1f01a03161e9b6f5bce
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-05-08 08:49:34 -0500

    caption bug, post order bug

--- lib/getHomePagePosts.ts
+++ lib/getHomePagePosts.ts
@@ -2,7 +2,7 @@ import client from './sanityClient'
 
 // Fetch all home page posts, ordered by published date
 export async function getHomePagePosts() {
-  return await client.fetch(`*[_type == "post"][0..8] | order(publishedAt desc) {
+  return await client.fetch(`*[_type == "post"] | order(publishedAt desc)[0..8] {
     _id,
     title,
     slug,
