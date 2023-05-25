4c47b176ce916529c991682117c87415d5dfb0f6
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-05-25 18:39:28 -0500

    scroll restoration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // TODO: This could break things, but good temporary fix for scroll restoration
+  experimental: { scrollRestoration: true },
   images: {
     remotePatterns: [
       {
