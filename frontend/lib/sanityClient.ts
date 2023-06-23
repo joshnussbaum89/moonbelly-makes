e25af749b505937c62eaaed871fab4282369c40e
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-06-23 15:12:16 -0500

    update sanity API token

--- frontend/lib/sanityClient.ts
+++ frontend/lib/sanityClient.ts
@@ -5,5 +5,5 @@ export default createClient({
   dataset: 'production',
   apiVersion: '2023-01-13',
   useCdn: false,
-  token: process.env.SANITY_API_TOKEN,
+  token: process.env.SANITY_API_WRITE_TOKEN,
 })
