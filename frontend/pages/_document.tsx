a068fd534edadb870115d07707e30cea16d471a7
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-07-31 16:48:15 -0500

    update Safari favicon

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -22,9 +22,9 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fffaff" />
-          <meta name="msapplication-TileColor" content="#fffaff" />
-          <meta name="theme-color" content="#fffaff"></meta>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#ffffff"></meta>
         </Head>
         <body>
           <Main />
