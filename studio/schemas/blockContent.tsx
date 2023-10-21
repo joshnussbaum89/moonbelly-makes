23903123affa7825fb8869ce79c0f9d05e374c25
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-10-21 13:47:31 -0500

    change h3 to h2 in posts

--- studio/schemas/blockContent.tsx
+++ studio/schemas/blockContent.tsx
@@ -15,6 +15,7 @@ export default defineType({
       type: 'block',
       styles: [
         {title: 'Normal', value: 'normal'},
+        {title: 'H2', value: 'h2'},
         {title: 'H3', value: 'h3'},
         {title: 'H4', value: 'h4'},
         {title: 'H5', value: 'h5'},
